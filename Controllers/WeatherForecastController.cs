275011904c8eb089c7f6d3408ae7e44b76cee49a
AselaDK <aselax2@gmail.com>
2021-09-29 11:12:13 +0530

    change 1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace CVer.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching111"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
